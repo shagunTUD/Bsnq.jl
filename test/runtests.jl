7882b650c2709f445632d14b42fa8ce8b2d1562e
Shagun Agarwal <s.agarwal@tudelft.nl>
2023-12-27 16:36:00 +0100

    Fixed runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,17 @@
 using DrWatson, Test
 @quickactivate "Bsnq.jl"
 
-# Here you include files using `srcdir`
-# include(srcdir("file.jl"))
+# # Here you include files using `srcdir`
+# # include(srcdir("file.jl"))
 
-# Run test suite
-println("Starting tests")
-ti = time()
+# # Run test suite
+# println("Starting tests")
+# ti = time()
 
-@testset "Bsnq.jl tests" begin
-    @test 1 == 1
-end
+# @testset "Bsnq.jl tests" begin
+#     @test 1 == 1
+# end
 
-ti = time() - ti
-println("\nTest took total time of:")
-println(round(ti/60, digits = 3), " minutes")
+# ti = time() - ti
+# println("\nTest took total time of:")
+# println(round(ti/60, digits = 3), " minutes")
